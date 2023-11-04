305a89d6326771e8cbbaf058f5060394610b2370
Abdulteef1998 <abdulte97@gmail.com>
2023-11-04 22:23:06 +0300

    create custom text field

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'custom_app_bar.dart';
+import 'notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -9,23 +9,13 @@ class NotesViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        children: const [
-          SizedBox(
-            height: 50,
-          ),
-          CustomAppBar()
-        ],
-      ),
+      child: Column(children: [
+        SizedBox(
+          height: 50,
+        ),
+        CustomAppBar(),
+        Expanded(child: NotesListView()),
+      ]),
     );
-  }
-}
-
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
