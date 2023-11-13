c96abf85d826b02b7fade068b11150ddd85377e1
Abdulteef1998 <abdulte97@gmail.com>
2023-11-13 14:18:03 +0300

    display notes part one

--- lib/cubits/notes_cubit.dart
+++ lib/cubits/notes_cubit.dart
@@ -8,15 +8,10 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
-  featchAllNotes() async {
-    emit(NotesLoading());
-    try {
-      var notesBox = Hive.box<NoteModel>(kNotesBox);
+  List<NoteModel>? notes;
+  fetchAllNotes() {
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
 
-      List<NoteModel> notes = notesBox.values.toList();
-      emit(NotesSuccess(notes));
-    } catch (e) {
-      emit(NotesFailure(e.toString()));
-    }
+    notes = notesBox.values.toList();
   }
 }
--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:noteapp/cubits/notes_cubit.dart';
+import 'package:noteapp/models/note_model.dart';
 import 'package:noteapp/views/widgets/note_item.dart';
 
 class NotesListView extends StatelessWidget {
@@ -6,16 +9,20 @@ class NotesListView extends StatelessWidget {
   final data = const [];
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 16),
-      child: ListView.builder(
-          padding: EdgeInsets.zero,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(vertical: 4),
-              child: NoteItem(),
-            );
-          }),
-    );
+    List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes ?? [];
+    return BlocBuilder<NotesCubit, NotesState>(builder: (context, state) {
+      return Padding(
+        padding: EdgeInsets.symmetric(vertical: 16),
+        child: ListView.builder(
+            itemCount: notes.length,
+            padding: EdgeInsets.zero,
+            itemBuilder: (context, index) {
+              return const Padding(
+                padding: EdgeInsets.symmetric(vertical: 4),
+                child: NoteItem(),
+              );
+            }),
+      );
+    });
   }
 }
