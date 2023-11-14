695da16d552140fc307470b54ca9fd289a900a5e
Abdulteef1998 <abdulte97@gmail.com>
2023-11-14 23:01:01 +0300

    colors list view part 2

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -53,6 +53,9 @@ class _AddNoteFormState extends State<AddNoteForm> {
             height: 32,
           ),
           const ColorsListView(),
+          const SizedBox(
+            height: 32,
+          ),
           BlocBuilder<AddNoteCubit, AddNoteState>(
             builder: (context, state) {
               return CustomButton(
